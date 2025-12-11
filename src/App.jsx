66d87013968d176875d59412be91846a7c93b32e
khashayarasghari <khasgharidev@gmail.com>
2025-12-11 16:10:29 +0330

    commit all files

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react"
+
+export default function App() {
+  const [number, setNumber] = useState(100)
+
+  console.log({number, setNumber})
+
+  function increment() {
+    setNumber(number + 1)
+    setNumber(number + 1)
+    setNumber(number + 1)
+    setNumber((prev) => {return prev + 1})
+    setNumber(prev => prev + 1)
+  }
+
+  function decrement() {
+    setNumber(number - 1)
+  }
+
+  return (
+    <>
+      <button onClick={increment}>+</button>
+      <span>{number}</span>
+      <button onClick={decrement}>-</button>
+    </>
+  )
+}
